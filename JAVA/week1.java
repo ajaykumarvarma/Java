aa8e13ab570abbc98c9bcf8dc515b4a1ca6c14f1
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2022-04-08 11:05:36 +0530

    Update week1.java

--- JAVA/week1.java
+++ JAVA/week1.java
@@ -23,29 +23,30 @@ System.out.println("Mean is: "+means);
   
   #program2 tofind prime numbers count in given 4digit number
   
-  import java.io.*;
+ import java.io.*;
 import java.util.*;
 public class prime
 {
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-int n,i,num,temp;
+int n,i;
 int flag=0;
 System.out.print("Enter 4 DigitNumber:");
 n=sc.nextInt();
-temp=n;
 int count=0;
 while(n!=0){
+flag=0;
 int rem=n%10;
 for(i=1;i<=rem;i++){
-if (rem%i==0){
+if(rem%i==0){
   flag=flag+1;
+}
 }
 if(flag==2){
 count+=1;
 }
-}
+
 n=n/10;
 }
 System.out.println("The number of primes in entered is: "+count);
