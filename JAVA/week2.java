b0b4abbaa5d6363ba8489af585f277ab5f0e0358
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2022-04-18 09:25:26 +0530

    Create week2.java

---
+++ JAVA/week2.java
@@ -0,0 +1,77 @@
+Menu driven calculator program
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Main {
+
+   
+    public static void main(String args[])
+   {
+      Scanner sc=new Scanner(System.in);
+      int x,y,choice,answer;
+      
+      while(true)
+      {
+      System.out.println("Choose one option from below:");
+      System.out.println("1.Addition");
+      System.out.println("2.Subtraction");
+      System.out.println("3.Multiplication");
+      System.out.println("4.Division");
+      System.out.println("5.Exit");
+          System.out.print("Enter your Choice:"); 
+          choice=sc.nextInt();
+          switch(choice)
+              {
+                    case 1:
+                       System.out.println("Enter Two Numbers for Addition:");
+                       System.out.print("Enter First Number:");
+                       x=sc.nextInt();
+                       
+                       System.out.print("Enter Second Number:");
+                       y=sc.nextInt();
+                       answer=x+y;
+                       System.out.println("Sum is:"+answer);
+                       break;
+                    case 2:
+                       System.out.println("Enter Two Numbers for Subtraction:");
+                       System.out.print("Enter First Number:");
+                       x=sc.nextInt();
+                       
+                       System.out.print("Enter Second Number:");
+                       y=sc.nextInt();
+                       answer=x-y;
+                       System.out.println("Difference is:"+answer);
+                       break;
+                    case 3:
+                       System.out.println("Enter Two Numbers for Multiplication:");
+                       System.out.print("Enter First Number:");
+                       x=sc.nextInt();
+                       
+                       System.out.print("Enter Second Number:");
+                       y=sc.nextInt();
+                       answer=x*y;
+                       System.out.println("Product is:"+answer);  
+                       break;
+                    case 4:
+                       System.out.println("Enter Two Numbers for Subtraction:");
+                       System.out.print("Enter First Number:");
+                       x=sc.nextInt();
+                       
+                       System.out.print("Enter Second Number:");
+                       y=sc.nextInt();
+                       answer=x/y;
+                       System.out.println("Division is:"+answer);     
+                       break;
+                    case 5:
+                        System.exit(0);
+                        
+                    default:
+                        System.out.println("Enter a valid Option"); 
+                        
+            } }}}
+
+-----------------------------------------------------------------------------------------------------------------------------
+  
+  
+  
