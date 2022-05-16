cfe6b32fcf2b0ea1158075001f3fd0454b983b39
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2022-05-16 09:17:57 +0530

    Create week5.java

---
+++ JAVA/week5.java
@@ -0,0 +1,18 @@
+QUESTIONS:
+
+1. Create a class with fileds sno, sname,smarks. Set the values to these fields with
+a. Default method or
+b. Parameterised method or
+c. Default Consrucor or
+d. Parameterised constructor
+Display the information using display() method
+----------------------------------------------------------------------------------------
+2. Create a class Employee withe fileds eno, ename, sal, city,pincode. Set the values using parameterised method. create another method to update the sal by Rs.1000. Print the information of employee before and after the update salary
+----------------------------------------------------------------------------------------
+3. Create a class Book with fields bno, bname, price, no_of_pages. Set the values using Parameterised constructor and then create a method to give discount of Rs. 100 and print the information before and after discount
+---------------------------------------------------------------------------------------------
+4. Create a class Vehicle with the fileds vno,vprice,model_name, price. Use
+a. overloaded methods or
+b. overloaded constructors to set values
+Print the information using a show() method
+-------------------------------------------------------------------------------------------
